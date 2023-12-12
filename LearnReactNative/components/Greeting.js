92f06afbf825b1c13f70a7108ec86e3fb2fd9a52
Younggyun <noxium@naver.com>
2023-12-12 20:46:37 +0900

    Initial commit 2

---
+++ LearnReactNative/components/Greeting.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+// function Greeting() {
+//   return (
+//     <View>
+//       <Text>안녕하세요 함수 컴포넌트!</Text>
+//     </View>
+//   );
+// }
+
+const Greeting = props => {
+  return (
+    <>
+      <View>
+        <Text>안녕하세요 {props.name}</Text>
+      </View>
+      <Text>Extra Text!</Text>
+    </>
+  );
+};
+
+Greeting.defaultProps = {
+  name: '리액트 네이티브',
+};
+
+export default Greeting;
