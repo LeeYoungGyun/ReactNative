79921ca3bd7e9f27f4d82b89fcabcbfc522e220c
Younggyun <noxium@naver.com>
2024-01-26 18:05:59 +0900

    feat: DayLog(#3)

--- DayLog/screens/CalendarScreen.js
+++ DayLog/screens/CalendarScreen.js
@@ -4,7 +4,6 @@ import {StyleSheet, View} from 'react-native';
 const CalendarScreen = () => {
   return <View style={styles.block} />;
 };
-
 const styles = StyleSheet.create({
   block: {},
 });
--- DayLog/screens/FeedsScreen.js
+++ DayLog/screens/FeedsScreen.js
@@ -1,16 +1,11 @@
-import React, {useContext} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-import LogContext from '../contexts/LogContext';
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
 
 const FeedsScreen = () => {
-  const value = useContext(LogContext);
-  return (
-    <View style={styles.block}>
-      <Text>{value}</Text>
-    </View>
-  );
+  return <View style={styles.block} />;
 };
-
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  block: {},
+});
 
 export default FeedsScreen;
